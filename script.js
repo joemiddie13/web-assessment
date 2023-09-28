8b8b964e1372a63679c903d9f4d2ffc2fe6ce4da
joemiddie13 <joemiddie13@gmail.com>
2023-09-28 11:55:26 -0700

    Working website!

--- script.js
+++ script.js
@@ -15,11 +15,17 @@ widthInput.addEventListener('input', () => {
 // Height Display in 3rd box
 heightInput.addEventListener('input', () => {
     displayHeight.textContent = heightInput.value;
-    colorBox.style.width = `${heightInput.value}px`;
+    colorBox.style.height = `${heightInput.value}px`;
 });
 
-// Color Display in 3rd box
+// Color Hex Value Display
 colorInput.addEventListener('input', () => {
     displayColor.textContent = colorInput.value;
     colorBox.style.backgroundColor = colorInput.value;
 });
+
+// Color Display Box in Container 4
+colorBox.style.display = 'inline-block';
+colorBox.style.width = '200px';
+colorBox.style.height = '200px';
+colorBox.style.backgroundColor = '#000000';
