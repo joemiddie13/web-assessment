4f8045f2e24fa2a725ac03f21572bda3ae46a2e9
joemiddie13 <joemiddie13@gmail.com>
2023-09-28 11:10:36 -0700

    Created EventListeners for 3rd container.

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const widthInput = document.getElementById('width-input');
-const heightInput = document.getElementById('height');
+const heightInput = document.getElementById('height-input');
+const colorInput = document.getElementById('color-input');
+const displayWidth = document.getElementById('displayWidth');
+const displayHeight = document.getElementById('displayHeight');
+const displayColor = document.getElementById('displayColor');
+const colorBox = document.getElementById('colorBox');
+
+// Width Display in 3rd box
+widthInput.addEventListener('input', () => {
+    displayWidth.textContent = widthInput.value;
+    colorBox.style.width = `${widthInput.value}px`;
+});
+
+// Height Display in 3rd box
+heightInput.addEventListener('input', () => {
+    displayHeight.textContent = heightInput.value;
+    colorBox.style.width = `${heightInput.value}px`;
+});
+
+// Color Display in 3rd box
+colorInput.addEventListener('input', () => {
+    displayColor.textContent = colorInput.value;
+    colorBox.style.backgroundColor = colorInput.value;
+});
